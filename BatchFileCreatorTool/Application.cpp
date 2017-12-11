07bd5a2725f5c4feda88b4d0fd63848666160531
pavelsakau <pashasbox@gmail.com>
2017-12-11 04:54:54 -0800

    New application icon

--- BatchFileCreatorTool/Application.cpp
+++ BatchFileCreatorTool/Application.cpp
@@ -1,4 +1,6 @@
 #include "Application.h"
+#include "BitmapHelper.h"
+#include "resource.h"
 
 bool Application::OnInit()
 {
@@ -7,6 +9,8 @@ bool Application::OnInit()
 	int desktopWidth = wxSystemSettings::GetMetric(wxSYS_SCREEN_X) ;
 	int desktopHeight = wxSystemSettings::GetMetric(wxSYS_SCREEN_Y) ;
 	mainWindow = new MainWindow(nullptr, wxID_ANY, "Batch File Creator Tool", wxDefaultPosition, wxSize(desktopWidth*0.55 > 900 ? desktopWidth*0.55: 900, desktopHeight*0.65));
+
+	mainWindow->SetIcon(wxIcon(BitmapHelper::GetResourceById(APPLICATIONICON), wxBITMAP_TYPE_ICO_RESOURCE));
 
 	mainWindow->Center();
 	mainWindow->Raise();
